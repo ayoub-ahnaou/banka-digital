6f0750813895bba405c03ae449e6b7de5257c605
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-12-17 00:48:24 +0100

    Add ApiResponse, ApiResponseError, and ApiResponseSuccess classes for standardized API responses

---
+++ src/main/java/com/digital/banka/dto/ApiResponse.java
@@ -0,0 +1,16 @@
+package com.digital.banka.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse {
+    private Integer status;
+    private String message;
+    private LocalDateTime timestamp;
+}
---
+++ src/main/java/com/digital/banka/dto/ApiResponseSuccess.java
@@ -0,0 +1,19 @@
+package com.digital.banka.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+public class ApiResponseSuccess<T> extends ApiResponse {
+    private T data;
+
+    public ApiResponseSuccess(Integer status, String message, T data) {
+        super(status, message, LocalDateTime.now());
+        this.data = data;
+    }
+}
