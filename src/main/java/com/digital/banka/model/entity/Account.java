fe6229b627a442bc15bc5f7b04b0d2bc2b220faf
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-12-16 22:51:23 +0100

    Add Account entity with JPA annotations and UUID generation for account numbers

---
+++ src/main/java/com/digital/banka/model/entity/Account.java
@@ -0,0 +1,42 @@
+package com.digital.banka.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "accounts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String accountNumber;
+
+    private Double balance;
+
+    @OneToOne(mappedBy = "account", cascade = CascadeType.ALL)
+    private User owner;
+
+    @OneToMany(mappedBy = "accountSource", cascade = CascadeType.ALL)
+    private List<Operation> operations;
+
+    @PrePersist
+    protected void onCreate() {
+        if (accountNumber == null) {
+            accountNumber = "BAR-" +
+                    UUID.randomUUID().toString().substring(0, 8).toUpperCase();
+        }
+    }
+}
