044adcc8a996712315ecc9e31ad375e7fd27185f
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-12-16 22:51:35 +0100

    Add Operation entity with JPA annotations and lifecycle management

---
+++ src/main/java/com/digital/banka/model/entity/Operation.java
@@ -0,0 +1,57 @@
+package com.digital.banka.model.entity;
+
+import com.digital.banka.model.enums.Status;
+import com.digital.banka.model.enums.Type;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "operations")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Operation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Type type;
+
+    private Double amount;
+
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.PENDING;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime validatedAt;
+    private LocalDateTime executedAt;
+
+    private Long accountDestination; // could be null in withdrawal or deposit operation
+
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account accountSource;
+
+    @OneToMany(mappedBy = "operation", cascade = CascadeType.ALL)
+    private List<Document> documents;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+
+        if (amount <= 10000.0 && type != Type.TRANSFER) {
+            status = Status.APPROVED;
+            validatedAt = LocalDateTime.now();
+            executedAt = LocalDateTime.now();
+        }
+    }
+}
