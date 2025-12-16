a89c0f9f9b794437502611a5de5a56585f564155
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-12-17 00:50:04 +0100

    Add AuthController for user registration endpoint

---
+++ src/main/java/com/digital/banka/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.digital.banka.controller;
+
+import com.digital.banka.dto.ApiResponse;
+import com.digital.banka.dto.ApiResponseSuccess;
+import com.digital.banka.dto.auth.request.RegisterRequest;
+import com.digital.banka.dto.auth.response.RegisterResponse;
+import com.digital.banka.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse> register(@Valid @RequestBody RegisterRequest request) {
+        RegisterResponse user = userService.createUser(request);
+        ApiResponseSuccess<RegisterResponse> res = new ApiResponseSuccess<>(201, "User registered successfully", user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
+}
