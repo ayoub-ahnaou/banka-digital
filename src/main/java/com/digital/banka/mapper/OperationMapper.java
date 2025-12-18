7770322c9d02e038df879fa1b0d66edfe50a0b3f
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-12-18 02:07:33 +0100

    Add mapping for WithdrawRequest in OperationMapper

--- src/main/java/com/digital/banka/mapper/OperationMapper.java
+++ src/main/java/com/digital/banka/mapper/OperationMapper.java
@@ -1,6 +1,7 @@
 package com.digital.banka.mapper;
 
 import com.digital.banka.dto.operation.request.DepositRequest;
+import com.digital.banka.dto.operation.request.WithdrawRequest;
 import com.digital.banka.dto.operation.response.OperationResponse;
 import com.digital.banka.model.entity.Operation;
 import org.mapstruct.Mapper;
@@ -21,4 +22,13 @@ public interface OperationMapper {
     @Mapping(target = "accountSource", ignore = true)
     @Mapping(target = "documents", ignore = true)
     Operation toEntity(DepositRequest dto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "status", expression = "java(com.digital.banka.model.enums.Status.PENDING)")
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "validatedAt", ignore = true)
+    @Mapping(target = "executedAt", ignore = true)
+    @Mapping(target = "accountSource", ignore = true)
+    @Mapping(target = "documents", ignore = true)
+    Operation toEntity(WithdrawRequest dto);
 }
