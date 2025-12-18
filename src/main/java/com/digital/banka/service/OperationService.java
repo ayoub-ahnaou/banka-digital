1684cee188f0a16dbbf88339f8394b02f5ea6dd6
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-12-18 02:07:43 +0100

    Implement withdraw functionality in OperationService and OperationServiceImpl

--- src/main/java/com/digital/banka/service/OperationService.java
+++ src/main/java/com/digital/banka/service/OperationService.java
@@ -1,8 +1,10 @@
 package com.digital.banka.service;
 
 import com.digital.banka.dto.operation.request.DepositRequest;
+import com.digital.banka.dto.operation.request.WithdrawRequest;
 import com.digital.banka.dto.operation.response.OperationResponse;
 
 public interface OperationService {
     OperationResponse deposit(DepositRequest request);
+    OperationResponse withdraw(WithdrawRequest request);
 }
