793f16d098ed41cb78e230077ef6c8aca53764ee
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-12-21 00:03:36 +0100

    Refactor UserController to simplify import statements and improve code readability

--- src/main/java/com/digital/banka/controller/admin/UserController.java
+++ src/main/java/com/digital/banka/controller/admin/UserController.java
@@ -5,10 +5,7 @@ import com.digital.banka.dto.ApiResponseSuccess;
 import com.digital.banka.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,11 +27,18 @@ public class UserController {
         ApiResponseSuccess<Object> res = new ApiResponseSuccess<>(200, "User account activated successfully", null);
         return ResponseEntity.ok().body(res);
     }
-    
+
     @PatchMapping("/{id}/promote")
     public ResponseEntity<ApiResponse> promoteUserToBankAgent(@PathVariable("id") Long id) {
         userService.promoteUserToBankAgent(id);
         ApiResponseSuccess<Object> res = new ApiResponseSuccess<>(200, "User promoted to bank agent successfully", null);
+        return ResponseEntity.ok().body(res);
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse> getAllUsers() {
+        var users = userService.getAllUsers();
+        ApiResponseSuccess<Object> res = new ApiResponseSuccess<>(200, "Users retrieved successfully", users);
         return ResponseEntity.ok().body(res);
     }
 }
