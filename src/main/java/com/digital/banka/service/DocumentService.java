1df2e08c68c8f6051c2a5b467cc90a16c725693d
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-12-20 16:51:03 +0100

    Implement getDocumentsByOrderId method in DocumentService and DocumentServiceImpl

--- src/main/java/com/digital/banka/service/DocumentService.java
+++ src/main/java/com/digital/banka/service/DocumentService.java
@@ -3,6 +3,9 @@ package com.digital.banka.service;
 import com.digital.banka.dto.document.request.UploadDocumentRequest;
 import com.digital.banka.dto.document.response.DocumentResponse;
 
+import java.util.List;
+
 public interface DocumentService {
     DocumentResponse uploadDocument(UploadDocumentRequest request, Long operationId);
+    List<DocumentResponse> getDocumentsByOrderId(Long operationId);
 }
