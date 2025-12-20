be080a7f8b387b30cf1cf4b22a75a13f98bead8e
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-12-20 16:50:57 +0100

    Add method to find documents by operation ID in DocumentRepository

--- src/main/java/com/digital/banka/repository/DocumentRepository.java
+++ src/main/java/com/digital/banka/repository/DocumentRepository.java
@@ -3,5 +3,8 @@ package com.digital.banka.repository;
 import com.digital.banka.model.entity.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DocumentRepository extends JpaRepository<Document, Long> {
+    List<Document> findByOperationId(Long operationId);
 }
