21b4f1cedf3b41f37f4daf7628dcbff5d6abe3ba
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-12-20 23:54:21 +0100

    Implement user account activation and deactivation endpoints in UserController

---
+++ src/main/java/com/digital/banka/controller/admin/UserController.java
@@ -0,0 +1,33 @@
+package com.digital.banka.controller.admin;
+
+import com.digital.banka.dto.ApiResponse;
+import com.digital.banka.dto.ApiResponseSuccess;
+import com.digital.banka.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @PatchMapping("/{id}/deactivate")
+    public ResponseEntity<ApiResponse> deactivateUserAccount(@PathVariable("id") Long id) {
+        userService.deactivateUserAccount(id);
+        ApiResponseSuccess<Object> res = new ApiResponseSuccess<>(200, "User account deactivated successfully", null);
+        return ResponseEntity.ok().body(res);
+    }
+
+    @PatchMapping("/{id}/activate")
+    public ResponseEntity<ApiResponse> activateUserAccount(@PathVariable("id") Long id) {
+        userService.activateUserAccount(id);
+        ApiResponseSuccess<Object> res = new ApiResponseSuccess<>(200, "User account activated successfully", null);
+        return ResponseEntity.ok().body(res);
+    }
+}
