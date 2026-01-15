444df09185305ad2e97a50f484ecd32b043f652e
ayoub-ahnaou <ayoubahnaou@gmail.com>
2026-01-15 14:26:19 +0100

    Add custom query method to OperationRepository for fetching operations by account source

--- src/main/java/com/digital/banka/repository/OperationRepository.java
+++ src/main/java/com/digital/banka/repository/OperationRepository.java
@@ -1,11 +1,17 @@
 package com.digital.banka.repository;
 
+import com.digital.banka.model.entity.Account;
 import com.digital.banka.model.entity.Operation;
 import com.digital.banka.model.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface OperationRepository extends JpaRepository<Operation, Long> {
     List<Operation> findOperationByStatus(Status status);
+
+    @Query("SELECT o FROM Operation o WHERE o.accountSource = :account")
+    List<Operation> findByAccountSource(@Param("account") Account account);
 }
